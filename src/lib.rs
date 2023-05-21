6b40a899f46004ab505d2d851d75f57695c46f20
Riccardo Persello <riccardo.persello@icloud.com>
2023-05-22 00:22:00 +0200

    Compiles on iPad and it's fucking fast.

--- src/lib.rs
+++ src/lib.rs
@@ -29,9 +29,7 @@ impl TypstCompiler {
     pub fn set_main(&self, main: String) -> Result<(), FileError> {
         if let Ok(mut world) = self.world.write() {
             let main_buf = world.root.join(main);
-            println!("Resolving main file \"{}\".", main_buf.display());
             world.main = world.resolve(&main_buf)?;
-            println!("Main file found.");
             Ok(())
         } else {
             panic!("Failed to lock world.")
@@ -41,7 +39,6 @@ impl TypstCompiler {
     pub fn compile(&self) -> CompilationResult {
         if let Ok(world) = self.world.read() {
             let result = typst::compile(&(*world));
-            println!("Compilation result: {:?}", result);
 
             if let Ok(doc) = result {
                 let pdf = typst::export::pdf(&doc);
